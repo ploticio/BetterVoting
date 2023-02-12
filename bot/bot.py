578086178edf076c5dc6e6e26864ecdc2de46811
Darren <darrentran001@gmail.com>
2023-02-11 18:25:58 -0800

    Project Restructure, sync command added

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from typing import Optional, Literal
 import settings
 
 def run_bot():
@@ -9,43 +10,58 @@ def run_bot():
     bot = commands.Bot(command_prefix="!", intents=intents)
 
     @bot.event
-    async def on_ready():
+    async def on_ready() -> None:
         print(f"{bot.user} is now running")
 
-        # for cogfile in settings.COGS_DIR.glob("*.py"):
-        #     if cogfile.name != "__init__.py":
-        #         await bot.load_extension(f"cogs.{cogfile.name[:-3]}")
-
-        await bot.load_extension("cogs.voting")
-        synced = await bot.tree.sync()
-        print(f"CMDs synced: {len(synced)}")
+    @bot.event
+    async def setup_hook() -> None:
+        for cogfile in settings.COGS_DIR.glob("*.py"):
+            if cogfile.name != "__init__.py":
+                await bot.load_extension(f"cogs.{cogfile.name[:-3]}")
 
 
     @bot.command(aliases=["p"], hidden=True)
-    async def ping(ctx):
+    async def ping(ctx: commands.Context) -> None:
         await ctx.send(f"{round(bot.latency * 1000, 1)}ms")
 
 
-    async def is_developer(ctx):
+    async def is_developer(ctx: commands.Context) -> None:
         return ctx.author.id == 147917812482965504
 
 
     @bot.command(hidden=True)
     @commands.check(is_developer)
-    async def load(ctx, cog: str):
+    async def load(ctx: commands.Context, cog: str) -> None: 
         await bot.load_extension(f"cogs.{cog}")
 
 
     @bot.command(hidden=True)
     @commands.check(is_developer)
-    async def unload(ctx, cog: str):
+    async def unload(ctx: commands.Context, cog: str) -> None:
         await bot.unload_extension(f"cogs.{cog}")
 
 
     @bot.command(hidden=True)
     @commands.check(is_developer)
-    async def reload(ctx, cog: str):
+    async def reload(ctx: commands.Context, cog: str) -> None:
         await bot.reload_extension(f"cogs.{cog}")
+
+    @bot.command(hidden=True)
+    @commands.check(is_developer)
+    async def sync(ctx: commands.Context, opt: Optional[Literal["*", "^", "~"]] = None):
+        if opt == "*":
+            synced = await bot.tree.sync()
+        elif opt == "^":
+            bot.tree.clear_commands(guild=ctx.guild)
+            await bot.tree.sync(guild=ctx.guild)
+            synced = []
+        elif opt == "~":
+            synced = await bot.tree.sync(guild=ctx.guild)
+        else:
+            bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await bot.tree.sync(guild=ctx.guild)
+        
+        await ctx.send(f"Synced {len(synced)} commands {'globally' if opt == '*' else 'locally'}")
 
 
     bot.run(settings.TOKEN)
--- bot/cogs/voting.py
+++ bot/cogs/voting.py
@@ -2,14 +2,23 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
-class Voting(commands.Cog):
-
+class Voting(commands.GroupCog, name="vote"):
+    
     def __init__(self, bot: commands.Bot) -> None:
+        super().__init__()
         self.bot = bot
 
-    @app_commands.command()
-    async def hello(self, interaction: discord.Interaction) -> None:
-        await interaction.response.send_message(content="Hello!")
+    @app_commands.command(name="plurality", description="Plurality Voting: Candidates with most votes wins. (May result in low voter satisfaction)")
+    async def plurality(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Plurality Vote")
+
+    @app_commands.command(name="ranked", description="Ranked Choice Voting: Voters rank candidates by preference")
+    async def rcv(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Ranked Choice Vote")
+
+    @app_commands.command(name="star", description="STAR Voting: (Recommended for highest voter satisfaction)")
+    async def star(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Star Vote")
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Voting(bot))
